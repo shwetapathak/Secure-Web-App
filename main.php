6edf0c0abf5e017633f615fdc8151cb3afba678f
shwetapathak <shwetupathak@gmail.com>
2016-11-09 06:43:36 -0600

    Update main.php

--- main.php
+++ main.php
@@ -1,14 +1,2 @@
-<html>
-<body>
-<h1> Sign up to post code : </h1>
-<form action= "main.php" enctype="multipart/form-data" method="post">
-    Username:  <input type="text" name = "username" value="username">
-    Password:   <input type="password" name = "password" value="password">
-    
-    <input type="submit" value="Send">
-	</form>
-
-	
-
-</body>
-</html>
+<?php
+  echo 'Hello, world!';
