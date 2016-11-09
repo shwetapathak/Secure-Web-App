47874375b6d373dabfa523370e5ffc14fb390377
shwetapathak <shwetupathak@gmail.com>
2016-11-08 18:59:46 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1>Home Page</H1><br>
+
+</form>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h1> Sign up to post pictures</h1>
+<form action= "main.php" enctype="multipart/form-data" method="post">
+    Username:  <input type="text" name = "username" value="username">
+    Password:   <input type="password" name = "password" value="password">
+    
+    <input type="submit" value="Send">
+</form>
+
+</body>
+</html>
